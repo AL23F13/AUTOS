62bcb0ad68bb6238e014157fd94aeb49b896a345
AL23F13 <davidmgyzt@gmail.com>
2015-06-23 08:59:44 -0500

    uno
    ask

---
+++ js/acciones.js
@@ -0,0 +1,27 @@
+// JavaScript Document
+$(document).ready(function(e) {
+document.addEventListener("deviceready",function(){
+	$ ('#btncomenzar') .on ('top' , function () { 
+  navigator .vibrate (1000);
+ });//  tap a btnvibrar
+	$ ('#principal') .on ('top' , function () { 
+  navigator .vibrate (1000);
+ });//  tap a btnvibrar
+ $ ('#page2') .on ('top' , function () { 
+  navigator .vibrate (1000);
+ });//  tap a btnvibrar
+ $ ('#page3') .on ('top' , function () { 
+  navigator .vibrate (1000);
+ });//  tap a btnvibrar
+ $ ('#page4') .on ('top' , function () { 
+  navigator .vibrate (1000);
+ });//  tap a btnvibrar
+ $ ('#page5') .on ('top' , function () { 
+  navigator .vibrate (1000);
+ });//  tap a btnvibrar
+ $ ('#pager6') .on ('top' , function () { 
+  navigator .vibrate (1000);
+ });//  tap a btnvibrar
+}); 
+});
+
